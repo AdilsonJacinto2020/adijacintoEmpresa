c8cc06c142a253c0a8197f6df37612b43b5aa7c6
ajacinto <ajacinto@Debian.myguest.virtualbox.org>
2026-05-07 12:55:42 +0100

    Refactor code structure for improved readability and maintainability

--- back/dist/main.js
+++ back/dist/main.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+const allowedOrigin = (process.env.FRONTEND_URL || 'http://localhost:5173').replace(/\/$/, '');
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
