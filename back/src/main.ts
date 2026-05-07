933e1afd84f0b32a2391275d3ce6e50296c0f1dc
ajacinto <ajacinto@Debian.myguest.virtualbox.org>
2026-05-07 13:47:56 +0100

    feat: update allowedOrigin handling and add Vercel configuration

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const allowedOrigin = (process.env.FRONTEND_URL || 'http://localhost:5173').replace(/\/$/, '');
+const allowedOrigin = (
+  process.env.FRONTEND_URL || 'http://localhost:5173'
+).replace(/\/$/, '');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
